4b03b657108128c1e8e1ba80b371eb4c5222da45
Daniel Stennett <Daniel.Stennett@farsound.com>
2025-05-23 11:37:00 +0100

    Develop Update Function to GUI

--- gui.py
+++ gui.py
@@ -4,21 +4,43 @@ import FreeSimpleGUI as gui
 label = gui.Text("Type in a To-Do")
 input_Box = gui.InputText(tooltip="Enter A To Do", key="todo")
 add_button = gui.Button("Add")
+current_label = gui.Text("List of Current Todos")
+list_box = gui.Listbox(values=functions.get_todos(),
+                       key="todos",
+                       enable_events=True,
+                       size=[45, 10])
+edit_button = gui.Button("Edit")
 
 window = gui.Window('My To Do App',
-                    layout=[[label], [input_Box, add_button]],
+                    layout=[[label], [input_Box, add_button],
+                            [current_label],
+                            [list_box, edit_button]],
                     font=("Helvetica", 20))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values["todos"])
+
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "Edit":
+            selected_todo = values["todos"][0]
+            updated_todo = values["todo"]
+            todos = functions.get_todos()
+            position = todos.index(selected_todo)
+            todos[position] = updated_todo
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value='')
+        case "todos":
+            window["todo"].update(values["todos"][0])
         case gui.WINDOW_CLOSED:
             break
 
