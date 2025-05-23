259dd22d1096b15e37017275150faf9152497b28
Daniel Stennett <Daniel.Stennett@farsound.com>
2025-05-23 10:39:18 +0100

    Develop Add Function to GUI

--- gui.py
+++ gui.py
@@ -2,9 +2,24 @@ import functions
 import FreeSimpleGUI as gui
 
 label = gui.Text("Type in a To-Do")
-input_Box = gui.InputText(tooltip="Enter A To Do")
+input_Box = gui.InputText(tooltip="Enter A To Do", key="todo")
 add_button = gui.Button("Add")
 
-window = gui.Window('My To Do App', layout=[[label], [input_Box, add_button]])
-window.read()
+window = gui.Window('My To Do App',
+                    layout=[[label], [input_Box, add_button]],
+                    font=("Helvetica", 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values["todo"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case gui.WINDOW_CLOSED:
+            break
+
 window.close()
