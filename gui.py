d22f8a3bac535609f0560545a8328d5bae0167f2
Daniel Stennett <Daniel.Stennett@farsound.com>
2025-05-26 11:35:07 +0100

    Develop Complete and Exit Functions to GUI

--- gui.py
+++ gui.py
@@ -10,11 +10,15 @@ list_box = gui.Listbox(values=functions.get_todos(),
                        enable_events=True,
                        size=[45, 10])
 edit_button = gui.Button("Edit")
+complete_button = gui.Button("Complete")
+exit_button = gui.Button("Exit")
 
 window = gui.Window('My To Do App',
-                    layout=[[label], [input_Box, add_button],
+                    layout=[[label],
+                            [input_Box, add_button],
                             [current_label],
-                            [list_box, edit_button]],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
                     font=("Helvetica", 20))
 
 while True:
@@ -38,7 +42,16 @@ while True:
             todos[position] = updated_todo
             functions.write_todos(todos)
             window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Complete":
+            complete_todo = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(complete_todo)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
             window["todo"].update(value='')
+        case "Exit":
+            break
         case "todos":
             window["todo"].update(values["todos"][0])
         case gui.WINDOW_CLOSED:
